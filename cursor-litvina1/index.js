89f545c69ee1e6e66a1f0ab61e7acc958b3e8119
Karina <litvina.k@ukr.net>
2022-08-24 10:24:23 +0300

    new

--- cursor-litvina1/index.js
+++ cursor-litvina1/index.js
@@ -7,65 +7,71 @@ const jeans = 90.2345;
 
 
 
-// max price
-const maxPrice = Math.max(dress, shoes, jeans)
-console.log(maxPrice)
+// // max price
+// const maxPrice = Math.max(dress, shoes, jeans)
+// console.log(maxPrice)
 
 
-// min price
-const minPrice = Math.min(dress, shoes, jeans)
-console.log(minPrice)
-
-
-
-// amount
-const calcAmount = dress + shoes + jeans;
-console.log(calcAmount)
-
-
-//amount, rounding down
-const calcAmountDown = Math.floor(dress) + Math.floor(shoes) + Math.floor(jeans);
-console.log(calcAmountDown)
-
-
-//the amount is rounded to hundreds
-const amountRoundHundreds = Math.ceil(calcAmountDown / 100) * 100
-console.log(amountRoundHundreds)
-
-
-//boolean 
-if (calcAmountDown % 2 === 0) {
-    console.log(true)
-} else {
-    console.log(false)
-}
-
-
-//surrender of money
-const surrenderMoney = 500 - (calcAmount)
-console.log(surrenderMoney)
+// // min price
+// const minPrice = Math.min(dress, shoes, jeans)
+// console.log(minPrice)
 
 
 
-//average value
-const value = calcAmount / 3;
-console.log(value.toFixed(2))
+// // amount
+// const calcAmount = dress + shoes + jeans;
+// console.log(calcAmount)
+
+
+// //amount, rounding down
+// const calcAmountDown = Math.floor(dress) + Math.floor(shoes) + Math.floor(jeans);
+// console.log(calcAmountDown)
+
+
+// //the amount is rounded to hundreds
+// const amountRoundHundreds = Math.ceil(calc2 / 100) * 100
+// console.log(amountRoundHundreds)
+
+
+// //boolean 
+// if (calcAmountDown % 2 === 0) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+
+// //surrender of money
+// const surrenderMoney = 500 - (calcAmount)
+// console.log(surrenderMoney)
+
+
+
+// //average value
+// const value = calcAmount / 3;
+// console.log(value.toFixed(2))
 
 
 //discount
 const discount = Math.random() * 25 + 5;
 
+console.log((dress - discount).toFixed(2));
 
-// Advanced
-const allInfo = {
-    MaxPrice: `${maxPrice}` ,
-    MinPrice: `${minPrice}` ,
-    Amount: `${calcAmount}` ,
-    AmountDown: `${calcAmountDown}` ,
-    AmountRoundHundreds: `${amountRoundHundreds}` ,
-    SurrenderofMoney: `${surrenderMoney}` ,
-    Averagevalue: `${value}`
 
-}
+// // Advanced
+// const allInfo = {
+//     MaxPrice: `${maxPrice}` ,
+//     MinPrice: `${minPrice}` ,
+//     Amount: `${calcAmount}` ,
+//     AmountDown: `${calcAmountDown}` ,
+//     AmountRoundHundreds: `${amountRoundHundreds}` ,
+//     SurrenderofMoney: `${surrenderMoney}`
+//     Averagevalue: `${value}`
 
-console.log(allInfo)
+// }
+
+// console.log(allInfo)
+
+
+
+
